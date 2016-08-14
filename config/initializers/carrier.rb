9305fbe6c3240640cf856a16de10b015cfa4e136
=mauman <=mauman@gmial.com>
2016-08-14 17:54:30 +0800

    inital

--- config/initializers/carrier.rb
+++ config/initializers/carrier.rb
@@ -1,9 +1,9 @@
 CarrierWave.configure do |config|
                         # required
   config.fog_credentials = {
-      :provider          =>     'AWS',                        # required
+      :provider          =>     'AWS',
       :aws_access_key_id =>   ENV['s3_secret_key']  ,                        # required
-      :aws_secret_access_key =>  ENV['s3_access_key'],                        # required
+      :aws_secret_access_key =>  ENV['s3_access_key'],
   }
   config.fog_directory  = ENV['s3_bucket']                        # required
 
